0e084a26c6b37f0b2b7bde7598162169f4c22fb6
vitmvit <120332119+vitmvit@users.noreply.github.com>
2024-01-20 01:22:34 +0300

    errorHandler

---
+++ src/main/java/ru/clevertec/house/controller/ExceptionHandlerController.java
@@ -0,0 +1,18 @@
+package ru.clevertec.house.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.clevertec.house.exception.EntityNotFoundException;
+import ru.clevertec.house.model.dto.ErrorDto;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorDto error(EntityNotFoundException e) {
+        return new ErrorDto(e.getMessage(), HttpStatus.NOT_FOUND.value());
+    }
+}
