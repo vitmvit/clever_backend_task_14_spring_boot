6f817438c5834374c0c7ecde46f5b29b9b6c784d
vitmvit <120332119+vitmvit@users.noreply.github.com>
2024-01-24 03:31:57 +0300

    refactor

--- src/main/java/ru/clevertec/house/constant/Constant.java
+++ src/main/java/ru/clevertec/house/constant/Constant.java
@@ -7,17 +7,9 @@ public class Constant {
     public static final String OFFSET_DEFAULT = "0";
     public static final String LIMIT_DEFAULT = "15";
 
-    public static final String NAME_DEFAULT = "";
-    public static final String SURNAME_DEFAULT = "";
-
     // cache
     public static final String LRU = "lru";
     public static final String LFU = "lfu";
     public static final float LOAD_FACTOR = 1.0f;
     public static final float CAPACITY_FACTOR = 1.5f;
-
-    // error
-    public static final String HOUSE_DELETE_ERROR = "House delete exception!";
-    public static final String HOUSE_UPDATE_ERROR = "House update exception!";
-    public static final String HOUSE_CREATE_ERROR = "House create exception!";
 }
